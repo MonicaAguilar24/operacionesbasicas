f867dd9b64467f19ca40835b899365c65c99728e
MonicaAguilar24 <145472036+MonicaAguilar24@users.noreply.github.com>
2023-09-26 09:20:19 -0600

    Add files via upload

---
+++ operaciones/basicas/OperacionesBasicas.java
@@ -0,0 +1,15 @@
+package operaciones.basicas;
+import javax.swing.JOptionPane;
+public class operaciones.basicas {
+public static void main(String [] args){
+ double ope1,ope2,r;
+ String ops1,ops2;
+ JOptionPane.showMessageDialog(null,"suma");
+ ops1=JOptionPane.showInputDialog("Ingresa el primer numero: ");
+ ope1=Double.parseDouble(ops1);
+ ope2=Double.parseDouble(ops2);
+ ope2=Double.parseDouble(ops2);
+ r=ope1+ope2;
+ JOptionPane.showMessageDialog(null,"suma="+r);
+    }
+}
